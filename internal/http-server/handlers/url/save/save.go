7451f96436cfd4ec6bea418c11096ee2e08407d3
Longin <l.khibovskiy@gmail.com>
2025-11-01 14:59:42 +0300

    update save.go

--- internal/http-server/handlers/url/save/save.go
+++ internal/http-server/handlers/url/save/save.go
@@ -1,12 +1,17 @@
 package save
 
+import resp "github.com/Longin-Khibovskiy/RestApiProject.git/internal/lib/api/response"
+
 type Request struct {
 	URL   string `json:"url" validate:"required,url"`
-	Alias string `json:"alias, omitempty""`
+	Alias string `json:"alias,omitempty"`
 }
 
 type Response struct {
-	Status string `json:"status"`
-	Error  string `json:"error, omitempty"`
-	Alias  string `json:"alias"`
+	resp.Response
+	Alias string `json:"alias"`
+}
+
+type URLSaver interface {
+	SaveURL(URL, alias string) (int64, error)
 }
